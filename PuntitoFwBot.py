bea3c87dbb09b46979871de3a757e710b5325897
PuntitOwO <69210614+PuntitOwO@users.noreply.github.com>
2020-08-04 15:37:40 -0400

    Add files via upload

---
+++ PuntitoFwBot.py
@@ -0,0 +1,27 @@
+import telebot
+bot = telebot.TeleBot("1164561903:AAGAhKaRU3QBSP9DKyrYZU7kqy5RCOA2KGc")
+
+canal_id="-1001285680844"
+personal_id="684613007"
+
+@bot.message_handler(commands=["start", "help"])
+def send_welcome(message):
+    bot.reply_to(message, "Hola! soy PuntitoForWardBot, simplemente utiliza /forward respondiendo a un mensaje para reenviarlo uwu")
+
+@bot.message_handler(commands=["forward"])
+def forwardeo(message):
+    if "group" in message.chat.type:
+        if message.reply_to_message is not None and not message.reply_to_message.from_user.is_bot:
+            bot.forward_message(message.chat.id, message.chat.id, message.reply_to_message.message_id)
+            bot.forward_message(canal_id, message.chat.id, message.reply_to_message.message_id)
+        else:
+            bot.reply_to(message, "El comando se usa citando un mensaje de otro usuario po, owo")
+    else:
+        bot.reply_to(message, "Disculpa, solo funciono en grupos uwu")
+
+@bot.message_handler(commands=["canal"])
+def send_canal(message):
+    bot.reply_to(message, "https://t.me/PuntitoForwarded")
+
+bot.polling(none_stop=True)
+
