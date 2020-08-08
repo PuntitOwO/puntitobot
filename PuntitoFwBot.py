88bf1b059cbc7eec2013c1e34bff8a16473ab826
PuntitOwO <69210614+PuntitOwO@users.noreply.github.com>
2020-08-07 21:14:01 -0400

    Update PuntitoFwBot.py

--- PuntitoFwBot.py
+++ PuntitoFwBot.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot(TOKEN)
 import sqlite3
 
 canal_id="-1001285680844"
-TABLE = "msg(msg_id integer primary key, original_sender interger, requested_by interger)"
+TABLE = "msg(msg_id integer primary key, original_sender integer, requested_by integer)"
 
 def create_table(table):
     con = sqlite3.connect('FwBot.db')
